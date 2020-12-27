c6f9b7087d88c2e9e20ccfab80dc5949d638f8cf
Facundo <facundofrosoni@gmail.com>
2020-12-26 23:34:52 -0300

    presentación proyecto 1

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -5,4 +5,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     accessToken: 'pk.eyJ1IjoiZXhlbnYiLCJhIjoiY2tpd2FhNHZsMGhscTJzcDNvOGwxZ2QzZCJ9.6bd-DlcEoc77tbdMpmN6xA'
 }).addTo(map);
 
-L.marker([-26.812386, -65.263015]).addTo(map);
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici) {
+            L.marker(bici.ubicacion, {tittle: bici.id}).addTo(map);
+        });
+    }
+})
